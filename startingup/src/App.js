08d97669d5e2569449a3664997d5f8b2a0a68d96
Imad <roulaimad7@gmail.com>
2023-11-23 15:36:48 +0100

    first

---
+++ startingup/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [number, setNumber] = useState(5);
+  function handleChange(x) {
+    if (x == "m" && number > 0) setNumber((prev) => prev - 1);
+    if (x == "p") setNumber((prev) => prev + 1);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={() => handleChange("m")}> - </button>
+      <h1> {number} </h1>
+      <button onClick={() => handleChange("p")}> + </button>
+    </div>
+  );
+}
+
+export default App;
